70270f613146ae84be384c7528efe3f548fd19dc
sahandsaeidi <sfrymswmh356@gmail.com>
2026-06-19 21:06:33 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,105 +1,67 @@
-function drag(e) {
-  e.dataTransfer.setData("code", e.target.dataset.code);
+function drag(event) {
+  // گرفتن کد بلوک
+  event.dataTransfer.setData("code", event.target.dataset.code);
 }
 
-function allowDrop(e) {
-  e.preventDefault();
+function allowDrop(event) {
+  // اجازه drop
+  event.preventDefault();
 }
 
-function drop(e) {
-  e.preventDefault();
+function drop(event) {
+  event.preventDefault();
 
-  const code = e.dataTransfer.getData("code");
+  const code = event.dataTransfer.getData("code");
 
+  // ساخت بلوک داخل workspace
   const div = document.createElement("div");
   div.className = "block";
+  div.dataset.code = code;
   div.innerText = code;
 
-  div.dataset.code = code;
-
+  // اضافه کردن به workspace
   document.querySelector(".workspace").appendChild(div);
 }
 
 function run() {
   let html = "";
-
-  document.querySelectorAll(".workspace .block").forEach(b => {
-    html += b.dataset.code + "\n";
-  });
-
-  document.getElementById("frame").srcdoc = html;
-}
-import { BLOCKS } from "./blocks/registry.js";
-
-// اضافه کردن بلوک به UI
-function createBlock(block, containerId) {
-  const div = document.createElement("div");
-  div.className = "block";
-  div.innerText = block.name;
-  div.dataset.code = block.code;
-
-  div.draggable = true;
-
-  div.ondragstart = (e) => {
-    e.dataTransfer.setData("code", block.code);
-  };
-
-  document.getElementById(containerId).appendChild(div);
-}
-
-// لود همه بلوک‌ها
-async function loadBlocks() {
-  for (let path of BLOCKS.html) {
-    const mod = await import(path);
-    createBlock(mod.default, "htmlBox");
-  }
-
-  for (let path of BLOCKS.css) {
-    const mod = await import(path);
-    createBlock(mod.default, "cssBox");
-  }
-
-  for (let path of BLOCKS.js) {
-    const mod = await import(path);
-    createBlock(mod.default, "jsBox");
-  }
-}
-
-loadBlocks();
-
-// workspace
-window.allowDrop = (e) => e.preventDefault();
-
-window.drop = (e) => {
-  e.preventDefault();
-
-  const code = e.dataTransfer.getData("code");
-
-  const div = document.createElement("div");
-  div.className = "block";
-  div.innerText = code;
-  div.dataset.code = code;
-
-  document.querySelector(".workspace").appendChild(div);
-};
-
-// run
-window.run = () => {
-  let html = "";
   let css = "";
   let js = "";
 
-  document.querySelectorAll(".workspace .block").forEach(b => {
-    const code = b.dataset.code;
+  // خواندن همه بلوک‌ها
+  document.querySelectorAll(".workspace .block").forEach(block => {
+    const code = block.dataset.code;
 
-    if (code.includes("<")) html += code + "\n";
-    else if (code.includes("{")) css += code + "\n";
-    else js += code + "\n";
+    // تشخیص ساده نوع کد
+    if (code.includes("<")) {
+      html += code + "\n";
+    } 
+    else if (code.includes("{")) {
+      css += code + "\n";
+    } 
+    else {
+      js += code + "\n";
+    }
   });
 
+  // اجرای خروجی داخل iframe
   document.getElementById("frame").srcdoc = `
-<style>${css}</style>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+${css}
+</style>
+</head>
+<body>
+
 ${html}
-<script>${js}<\/script>
-`;
-};
+
+<script>
+${js}
+<\/script>
+
+</body>
+</html>
+  `;
+}
