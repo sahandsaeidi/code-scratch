95512ab73fe544c38746f2ad7c998b37ba58223b
sahandsaeidi <sfrymswmh356@gmail.com>
2026-06-19 21:26:55 +0330

    Update registry.js

--- registry.js
+++ registry.js
@@ -1,7 +1,7 @@
 export const BLOCKS = {
   html: [
     "https://sahandsaeidi.github.io/html-scratch/h1.js",
-    "https://sahandsaeidi.github.io/html-scratch/ح.js",
+    "https://sahandsaeidi.github.io/html-scratch/p.js",
     "https://sahandsaeidi.github.io/html-scratch/button.js"
   ],
   css: [
