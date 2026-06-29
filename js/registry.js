885ae3e054a5627dd56b31177fc8ffa6908e73c2
sahandsaeidi <sfrymswmh356@gmail.com>
2026-06-29 12:07:30 +0330

    Add files via upload

---
+++ js/registry.js
@@ -0,0 +1,13 @@
+export const BLOCKS = {
+  html: [
+    "./js/blocks/html/h1.js",
+    "./js/blocks/html/p.js",
+    "./js/blocks/html/button.js"
+  ],
+  css: [
+    "./js/blocks/css/color.js"
+  ],
+  js: [
+    "./js/blocks/js/alert.js"
+  ]
+};
